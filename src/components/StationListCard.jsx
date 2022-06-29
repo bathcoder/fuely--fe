eeb7b2986b084c87ca8aebd976e8d2c7d15c38ec
abath257 <abath257@gmail.com>
2022-06-29 13:04:18 +0100

    Working on the functionality for update price

--- src/components/StationListCard.jsx
+++ src/components/StationListCard.jsx
@@ -1,11 +1,13 @@
-import {useEffec}
+import { useState } from "react";
 
 const StationListCard = ({ station }) => {
-const[newPrice, setNewPrice] = useState(station.price_petrol_main);
-
-
-
-
+  const [newPrice, setNewPrice] = useState("");
+console.log(station.price_petrol_main)
+console.log(newPrice)
+const handleSubmit = (event) => {
+  event.preventDefault();
+  station.price_petrol_main = newPrice;
+}
 
 
   return (
@@ -17,21 +19,26 @@ const[newPrice, setNewPrice] = useState(station.price_petrol_main);
           {<h3>{station.distance_from_center} miles</h3>}
         </div>
         <div className="station-prices">
-        {<h1>{station.price_petrol_main}</h1>}
-        {<h3>{station.updated}</h3>}
+          {<h1>{station.price_petrol_main}</h1>}
+          {<h3>{station.updated}</h3>}
+          <form onSubmit ={handleSubmit}>
+            <label>
+              <input
+                type="text"
+                id="price-input"
+                name="new-price"
+                placeholder="what price can you see?"
+                value={newPrice}
+                onChange={(event) => setNewPrice(event.target.value)}
+              />
+            </label>
+       
+          </form>
+          <button type="submit">enter fuely</button>
         </div>
-      </div>
-      <div>
-        <form>
-          <label>
-            <
-          </label>
-        </form>
       </div>
     </>
   );
 };
 
 export default StationListCard;
-
-
