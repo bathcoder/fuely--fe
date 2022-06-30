70ed437ed14e4bff8a13e602281bd25d96d8e557
pkaodev <polsson.development@gmail.com>
2022-06-30 12:10:24 +0100

    started adding firebase auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-
 import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
+import TEST from './components/TEST';
+
+import { useState, useEffect } from 'react';
+import {auth} from './services/firebase';
+import Login from './components/Login';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      setUser(user);
+    })
+
+    //old
+    // firebase.auth().onAuthStateChanged(user => {
+    //   setUser(user);
+    // })
+  }, [])
+
+  console.log(user);
+
+
   return (
  <div>
-<Header />
-<Home />
+  {user ? <TEST user={user} /> : <Login />}
+{/* <Header />
+<Home /> */}
     </div>
   );
 }
---
+++ src/services/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDPhq825816qcF27H4cYkUrW7Hp4tjPHd0",
+    authDomain: "fuely-8380e.firebaseapp.com",
+    projectId: "fuely-8380e",
+    storageBucket: "fuely-8380e.appspot.com",
+    messagingSenderId: "864088807443",
+    appId: "1:864088807443:web:2aeea0f05242877ffe87e6"
+  };
+
+// Initialize Firebase 
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
