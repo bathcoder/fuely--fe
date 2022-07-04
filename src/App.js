bac39e12c56ce27357d7fc314160b4c03e60a394
baldock92 <george_baldock@hotmail.co.uk>
2022-07-04 16:20:55 +0100

    login and create account features added

--- src/App.js
+++ src/App.js
@@ -7,20 +7,38 @@ import Signup from "./firebaseauth/Signup";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "./firebaseauth/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <>
-     <AuthProvider>
       <Container
         className="d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh" }}
-        >
+      >
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <PrivateRoute>
+                      <Dashboard />
+                    </PrivateRoute>
+                  }
+                ></Route>
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </AuthProvider>
+          </Router>
+          {/* <Signup /> */}
         </div>
       </Container>
-    </AuthProvider>
     </>
   );
 
